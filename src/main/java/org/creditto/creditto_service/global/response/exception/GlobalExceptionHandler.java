ba4ac3317acb35baea066206dec157e62cc3602e
Jeyong <dlwpdyd201@naver.com>
2025-11-17 10:37:38 +0900

    refactor: ErrorBaseCode 수정으로 인한 수정

--- src/main/java/org/creditto/creditto_service/global/response/exception/GlobalExceptionHandler.java
+++ src/main/java/org/creditto/creditto_service/global/response/exception/GlobalExceptionHandler.java
@@ -57,7 +57,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<BaseResponse<?>> handleIllegalArgumentException(final IllegalArgumentException e) {
         logWarn(e);
-        return ApiResponseUtil.failure(ErrorBaseCode.BAD_REQUEST_ILLEGALARGUMENTS, e.getMessage());
+        return ApiResponseUtil.failure(ErrorBaseCode.BAD_REQUEST, e.getMessage());
     }
 
     /**
