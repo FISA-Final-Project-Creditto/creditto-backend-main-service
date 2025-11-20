bdf00ca7a38c06caf0a6b790243aa73750754130
Jeyong <dlwpdyd201@naver.com>
2025-11-20 15:03:07 +0900

    fix: ExternalUserIdResolver를 WebConfig의 Resolver로 추가

---
+++ src/main/java/org/creditto/creditto_service/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.creditto.creditto_service.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.creditto.creditto_service.global.resolver.ExternalUserIdResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ExternalUserIdResolver externalUserIdResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(externalUserIdResolver);
+    }
+}
