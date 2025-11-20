cc51bdd02bfe5fcbc3cccfa6ce24b647518e5b93
kswdot <rlatjddms7a@naver.com>
2025-11-20 12:08:18 +0900

    refactor: controller 반환 타입 수정

--- src/main/java/org/creditto/creditto_service/domain/account/controller/AccountController.java
+++ src/main/java/org/creditto/creditto_service/domain/account/controller/AccountController.java
@@ -1,20 +1,26 @@
 package org.creditto.creditto_service.domain.account.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.creditto.creditto_service.domain.account.dto.CreateAccountReq;
 import org.creditto.creditto_service.domain.account.service.AccountService;
 import org.creditto.creditto_service.domain.overseasRemittance.dto.OverseasRemittanceReq;
 import org.creditto.creditto_service.domain.overseasRemittance.service.OverseasRemittanceService;
 import org.creditto.creditto_service.domain.transaction.service.TransactionService;
 import org.creditto.creditto_service.global.infra.corebanking.AccountRes;
+import org.creditto.creditto_service.global.infra.corebanking.OverseasRemittanceRes;
+import org.creditto.creditto_service.global.infra.corebanking.TransactionRes;
 import org.creditto.creditto_service.global.resolver.ExternalUserId;
 import org.creditto.creditto_service.global.response.ApiResponseUtil;
+import org.creditto.creditto_service.global.response.BaseResponse;
 import org.creditto.creditto_service.global.response.SuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/accounts")
@@ -25,7 +31,7 @@ public class AccountController {
     private final OverseasRemittanceService overseasRemittanceService;
 
     @PostMapping
-    public ResponseEntity<?> createAccount(
+    public ResponseEntity<BaseResponse<AccountRes>> createAccount(
             @ExternalUserId String userId,
             @RequestBody CreateAccountReq request
     ) {
@@ -34,15 +40,18 @@ public class AccountController {
 
     // 계좌 조회 by accountId
     @GetMapping("/id/{accountId}")
-    public ResponseEntity<?> getAccountByAccountId(
+    public ResponseEntity<BaseResponse<AccountRes>> getAccountByAccountId(
             @PathVariable("accountId") Long accountId
     ) {
-        return ApiResponseUtil.success(SuccessCode.OK, accountService.getAccountByAccountId(accountId));
+        log.info("getAccountByAccountId {}", accountId);
+        AccountRes accountRes = accountService.getAccountByAccountId(accountId);
+        log.info("조회된 계좌 정보: {}", accountRes); // 로그 메시지도 조금 더 명확하게 할 수 있습니다.
+        return ApiResponseUtil.success(SuccessCode.OK, accountRes); // accountRes 변수 재사용
     }
 
     // 계좌 잔액 조회 by accountId
     @GetMapping("/id/{accountId}/balance")
-    public ResponseEntity<?> getBalanceByAccountId(
+    public ResponseEntity<BaseResponse<BigDecimal>> getBalanceByAccountId(
             @PathVariable("accountId") Long accountId
     ) {
         return ApiResponseUtil.success(SuccessCode.OK, accountService.getAccountBalanceByAccountId(accountId));
@@ -51,7 +60,7 @@ public class AccountController {
 
     // 계좌 조회 by accountNo
     @GetMapping("/number/{accountNo}")
-    public ResponseEntity<?> getAccountByAccountNo(
+    public ResponseEntity<BaseResponse<AccountRes>> getAccountByAccountNo(
             @PathVariable("accountNo") String accountNo
     ) {
         return ApiResponseUtil.success(SuccessCode.OK, accountService.getAccountByAccountNo(accountNo));
@@ -59,7 +68,7 @@ public class AccountController {
 
     // 전체 계좌 조회 by userId
     @GetMapping("/me/accounts")
-    public ResponseEntity<?> getAccountByUserId(
+    public ResponseEntity<BaseResponse<AccountRes>> getAccountByUserId(
             @ExternalUserId String userId
     ) {
         return ApiResponseUtil.success(SuccessCode.OK, accountService.getAccountByExternalUserId(userId));
@@ -67,7 +76,7 @@ public class AccountController {
 
     // 거래 내역 조회 by accountId
     @GetMapping("/id/{accountId}/transactions")
-    public ResponseEntity<?> getTransactionsByAccountId(
+    public ResponseEntity<BaseResponse<List<TransactionRes>>> getTransactionsByAccountId(
             @PathVariable("accountId")  Long accountId
     ) {
         return ApiResponseUtil.success(SuccessCode.OK, transactionService.getTransactionByAccountId(accountId));
@@ -75,11 +84,11 @@ public class AccountController {
 
     // 일회성 해외 송금 등록
     @PostMapping("/{accountId}/remittance/once")
-    public ResponseEntity<?> remittanceOnce(
+    public ResponseEntity<BaseResponse<OverseasRemittanceRes>> remittanceOnce(
             @ExternalUserId String userId,
             @PathVariable("accountId") Long accountId,
             @RequestBody OverseasRemittanceReq request
-            ) {
+    ) {
         return ApiResponseUtil.success(SuccessCode.OK, overseasRemittanceService.processRemittanceOnce(userId, accountId, request));
     }
 
