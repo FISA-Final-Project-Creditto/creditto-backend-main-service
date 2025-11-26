e075d6fd834ac6c349f6d1fd756ce618d4fe96ba
wo-oaw <wo-oaw@naver.com>
2025-11-26 16:07:36 +0900

    feat: 정기 송금 관련 테스트코드 작성 및 사용하지 않는 dto 정리

--- src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignClient.java
+++ src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignClient.java
@@ -53,7 +53,7 @@ public interface CoreBankingFeignClient {
 
     // 사용자 정기송금 설정 내역 조회
     @GetMapping("/api/core/remittance/schedule")
-    List<RegularRemittanceDto> getScheduledRemittancesByUserId(@RequestParam("userId") String userId);
+    List<RegularRemittanceResponseDto> getScheduledRemittancesByUserId(@RequestParam("userId") Long userId);
 
     // 한 건의 정기 해외 송금 설정 내역 조회
     @GetMapping("/api/core/remittance/schedule/{regRemId}")
