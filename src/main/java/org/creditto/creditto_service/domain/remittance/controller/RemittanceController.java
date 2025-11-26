1c4d1bd0324839d2a542d07e62a3afbf2bfc62af
wo-oaw <wo-oaw@naver.com>
2025-11-26 16:23:01 +0900

    fix: 누락된 엔드포인트 및 사용하지 않는 참조 정리

--- src/main/java/org/creditto/creditto_service/domain/remittance/controller/RemittanceController.java
+++ src/main/java/org/creditto/creditto_service/domain/remittance/controller/RemittanceController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/remittance")
+@RequestMapping("/api/remittance/scheduled")
 public class RemittanceController {
 
     private final RemittanceService remittanceService;
