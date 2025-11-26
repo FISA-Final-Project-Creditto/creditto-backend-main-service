f7d298ff1f07b5ca55b6c271b5e8b8922bc92533
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-11-26 16:57:40 +0900

    chore: Account 관련 엔드포인트 수정 / #45 (#46)
    * chore: Account 관련 엔드포인트 수정
    
    * chore: Account 관련 엔드포인트 수정 (list -> me)

--- src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignClient.java
+++ src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignClient.java
@@ -5,10 +5,7 @@ import org.creditto.creditto_service.domain.overseasRemittance.dto.OverseasRemit
 import org.creditto.creditto_service.global.common.CoreBankingRes;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -19,24 +16,33 @@ import java.util.List;
         configuration = CoreBankingFeignConfig.class
 )
 public interface CoreBankingFeignClient {
+    /*
+    ACCOUNT
+     */
     @PostMapping(value = "/api/core/account/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     CoreBankingRes<AccountRes> createAccount(@PathVariable Long userId, @RequestBody CreateAccountReq request);
 
-    @GetMapping(value = "/api/core/account/{accountId}/account")
+    @GetMapping(value = "/api/core/account/{accountId}")
     CoreBankingRes<AccountRes> getAccountByAccountId(@PathVariable Long accountId);
 
     @GetMapping(value = "/api/core/account/{accountId}/balance")
     CoreBankingRes<BigDecimal> getAccountBalanceByAccountId(@PathVariable Long accountId);
 
-    @GetMapping(value = "/api/core/account/{accountNo}")
-    CoreBankingRes<AccountRes> getAccountByAccountNo(@PathVariable String accountNo);
+    @GetMapping(value = "/api/core/account")
+    CoreBankingRes<AccountRes> getAccountByAccountNo(@RequestParam(name = "accountNo") String accountNo);
 
-    @GetMapping(value = "/api/core/account/client/{userId}")
-    CoreBankingRes<List<AccountRes>> getAccountByUserId(@PathVariable Long userId);
+    @GetMapping(value = "/api/core/account")
+    CoreBankingRes<List<AccountRes>> getAccountsByUserId(@RequestParam(name = "userId") Long userId);
 
+    /*
+    TRANSACTION
+     */
     @GetMapping(value = "/api/core/transactions/{accountId}")
     CoreBankingRes<List<TransactionRes>> getTransactionByAccountId(@PathVariable Long accountId);
 
+    /*
+    REMITTANCE
+     */
     @PostMapping(value = "/api/core/remittance/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     CoreBankingRes<OverseasRemittanceRes> processRemittanceOnce(@PathVariable Long userId, @RequestBody OverseasRemittanceReq request);
 
