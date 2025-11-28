13ead0931b31832dcc880378b9f4781ebbcdc6a9
wo-oaw <wo-oaw@naver.com>
2025-11-28 17:17:21 +0900

    feat: 특정 정기송금의 세부사항 조회 API 구현 및 Dto 정리

---
+++ src/main/java/org/creditto/creditto_service/domain/remittance/dto/RemittanceHistoryDetailDto.java
@@ -0,0 +1,17 @@
+package org.creditto.creditto_service.domain.remittance.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RemittanceHistoryDetailDto {
+    private String accountNo;
+    private BigDecimal totalFee;
+    private BigDecimal sendAmount;
+    private String recipientBankName;
+    private String recipientAccountNo;
+    private String remittanceStatus;
+}
