9d888b6088f8270ce387e33b6c1a0185198c13b6
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-11-28 19:26:23 +0900

    fix/ Zipkin OpenFeign 사용시 traceId 미전파 문제 해결 /  #71  (#72)
    * build: feign-micrometer 의존성 추가
    
    * build: traceId를 위해 FeignConfig내 Feign Builder micrometer 설정 추가
    
    * docs: sql 쿼리 로깅 해제
    
    * refactor: MicrometerObservationCapability만 bean으로 등록하도록 수정

--- src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignConfig.java
+++ src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignConfig.java
@@ -2,14 +2,23 @@ package org.creditto.creditto_service.global.infra.corebanking;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.codec.ErrorDecoder;
+import feign.micrometer.MicrometerObservationCapability;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class CoreBankingFeignConfig {
 
     @Bean
     public ErrorDecoder coreBankingFeignErrorDecoder(ObjectMapper objectMapper) {
         return new CoreBankingFeignErrorDecoder(objectMapper);
+    }
+
+    @Bean
+    MicrometerObservationCapability micrometerObservationCapability(ObservationRegistry registry) {
+        return new MicrometerObservationCapability(registry);
     }
 }
