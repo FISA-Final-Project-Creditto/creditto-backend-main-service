6d49e4f13df23466b2202e1ab87a40014bd6fb5b
Jeyong <dlwpdyd201@naver.com>
2025-11-28 19:15:06 +0900

    build: traceId를 위해 FeignConfig내 Feign Builder micrometer 설정 추가

--- src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignConfig.java
+++ src/main/java/org/creditto/creditto_service/global/infra/corebanking/CoreBankingFeignConfig.java
@@ -1,15 +1,34 @@
 package org.creditto.creditto_service.global.infra.corebanking;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
 import feign.codec.ErrorDecoder;
+import feign.micrometer.MicrometerObservationCapability;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class CoreBankingFeignConfig {
+
+    private final ObservationRegistry observationRegistry;
+
+    @Value("${CORE_BANKING_SERVER_URL}")
+    private String coreBankingUrl;
 
     @Bean
     public ErrorDecoder coreBankingFeignErrorDecoder(ObjectMapper objectMapper) {
         return new CoreBankingFeignErrorDecoder(objectMapper);
+    }
+
+    @Bean
+    public CoreBankingFeignClient coreBankingFeignClient(ErrorDecoder coreBankingFeignErrorDecoder) {
+        return Feign.builder()
+                .errorDecoder(coreBankingFeignErrorDecoder)
+                .addCapability(new MicrometerObservationCapability(observationRegistry))
+                .target(CoreBankingFeignClient.class, coreBankingUrl);
     }
 }
