e36416782e39261146f8887f4f68d7b4b1133568
kswdot <rlatjddms7a@naver.com>
2025-12-01 17:52:05 +0900

    refactor: DB 저장 및 API 반환값 수정

--- src/main/java/org/creditto/creditto_service/global/infra/corebanking/TransactionRes.java
+++ src/main/java/org/creditto/creditto_service/global/infra/corebanking/TransactionRes.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record TransactionRes(
+        Long txnId,
         Long accountId,
         BigDecimal txnAmount,
         TxnType txnType,
