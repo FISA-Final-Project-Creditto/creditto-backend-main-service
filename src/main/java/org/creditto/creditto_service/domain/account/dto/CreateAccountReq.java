2827dfaa805f20ccc45e5d69bfb65604304de627
kswdot <rlatjddms7a@naver.com>
2025-12-02 12:10:06 +0900

    refactor: 계좌 비밀번호, 비밀번호 확인 검증

--- src/main/java/org/creditto/creditto_service/domain/account/dto/CreateAccountReq.java
+++ src/main/java/org/creditto/creditto_service/domain/account/dto/CreateAccountReq.java
@@ -1,8 +1,18 @@
 package org.creditto.creditto_service.domain.account.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.creditto.creditto_service.domain.account.enums.AccountType;
 
 public record CreateAccountReq(
         String accountName,
-        AccountType accountType
+        AccountType accountType,
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Pattern(regexp = "^\\d{4}$", message = "비밀번호는 4자리 숫자여야 합니다.")
+        String password,
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Pattern(regexp = "^\\d{4}$", message = "비밀번호는 4자리 숫자여야 합니다.")
+        String passwordConfirmation
 ) { }
