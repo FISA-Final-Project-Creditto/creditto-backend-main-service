872bf58a18d2912039674b490701b0ae36b824e5
Seongeun Kim <102601303+kswdot@users.noreply.github.com>
2025-12-03 17:32:52 +0900

    feat: OpenFeign 타임아웃, 재시도 설정 (#119)
    * feat: OpenFeign 타임아웃, 재시도 설정
    
    * feat: OpenFeign 재시도 횟수 수정

--- src/main/java/org/creditto/creditto_service/global/config/FeignConfig.java
+++ src/main/java/org/creditto/creditto_service/global/config/FeignConfig.java
@@ -1,8 +1,19 @@
 package org.creditto.creditto_service.global.config;
 
+import feign.Retryer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableFeignClients(basePackages = "org.creditto.creditto_service.global.infra")
-public class FeignConfig {}
+public class FeignConfig {
+
+    @Bean
+    Retryer.Default retryer() {
+        // 최초 요청 포함 최대 3번 시도
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(1L), 3);
+    }
+}
