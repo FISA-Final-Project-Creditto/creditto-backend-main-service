83b0367877ee0eae11eeefd00fbb402313dff63d
kswdot <rlatjddms7a@naver.com>
2025-12-03 17:36:42 +0900

    feat: 환전 우대율 조회 시 우대 환율 반환값 추가

--- src/main/java/org/creditto/creditto_service/global/infra/corebanking/PreferentialRateRes.java
+++ src/main/java/org/creditto/creditto_service/global/infra/corebanking/PreferentialRateRes.java
@@ -1,6 +1,9 @@
 package org.creditto.creditto_service.global.infra.corebanking;
 
+import java.math.BigDecimal;
+
 public record PreferentialRateRes(
-        double preferentialRate
+        double preferentialRate,
+        BigDecimal appliedRate
 ) {
 }
